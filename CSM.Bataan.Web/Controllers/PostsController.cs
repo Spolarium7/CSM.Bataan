a16e0b4794b3417432bf2c9893af251fd5145beb
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-08-24 18:39:04 +0800

    Database Setup

--- CSM.Bataan.Web/Controllers/PostsController.cs
+++ CSM.Bataan.Web/Controllers/PostsController.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSM.Bataan.Web.Infrastructure.Data.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSM.Bataan.Web.Controllers
 {
     public class PostsController : Controller
     {
+        private readonly DefaultDbContext _context;
+
+        public PostsController(DefaultDbContext context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
+            var posts = this._context.Posts.ToList();
+
             return View();
         }
 
---
+++ CSM.Bataan.Web/Migrations/20180824102824_init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSM.Bataan.Web.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Timestamp = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    Thumbnail = table.Column<string>(nullable: true),
+                    TemplateName = table.Column<string>(nullable: true),
+                    IsPublished = table.Column<bool>(nullable: false),
+                    PostExpiry = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
