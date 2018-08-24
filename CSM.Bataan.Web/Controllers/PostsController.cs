b66a8f3ab1424dffe5d8de4211f658ff9237a08d
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-08-24 17:56:05 +0800

    Initial Commit

---
+++ CSM.Bataan.Web/Controllers/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSM.Bataan.Web.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post(Guid? postId)
+        {
+            return View();
+        }
+
+        public IActionResult Carousel()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
