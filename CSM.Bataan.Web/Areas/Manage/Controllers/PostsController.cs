2d7579ca60fe93b4755f39fc7714cb1866655c7e
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-08-31 18:19:16 +0800

    Create post

---
+++ CSM.Bataan.Web/Areas/Manage/Controllers/PostsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSM.Bataan.Web.Areas.Manage.ViewModels.Posts;
+using CSM.Bataan.Web.Infrastructure.Data.Helpers;
+using CSM.Bataan.Web.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSM.Bataan.Web.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class PostsController : Controller
+    {
+        private readonly DefaultDbContext _context;
+
+        public PostsController(DefaultDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet, Route("manage/posts/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("manage/posts/create")]
+        public IActionResult Create(CreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            Post post = new Post()
+            {
+                Id = Guid.NewGuid(),
+                Content = model.Content,
+                Description = model.Description,
+                IsPublished = true,
+                PostExpiry = model.PostExpiry,
+                TemplateName = "post1",
+                Title = model.Title
+            };
+
+            this._context.Posts.Add(post);
+            this._context.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ CSM.Bataan.Web/Areas/Manage/ViewModels/Posts/CreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSM.Bataan.Web.Areas.Manage.ViewModels.Posts
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime PostExpiry { get; set; }
+    }
+}
