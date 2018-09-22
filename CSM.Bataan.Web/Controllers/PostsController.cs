9f2082e3606a6179020a78c88bc364273d90a047
Goshen Jimenez <GoshenJimenez_BlueSkyPh>
2018-09-22 20:52:34 +0800

    View Post using BBCode Parser from Code plex

--- CSM.Bataan.Web/Controllers/PostsController.cs
+++ CSM.Bataan.Web/Controllers/PostsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeKicker.BBCode;
 using CSM.Bataan.Web.Infrastructure.Data.Helpers;
 using CSM.Bataan.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -26,9 +27,41 @@ namespace CSM.Bataan.Web.Controllers
             });
         }
 
+        [HttpGet, Route("/posts/{postId}")]
         public IActionResult Post(Guid? postId)
         {
-            return View();
+            var post = this._context.Posts.FirstOrDefault(p => p.Id == postId);
+
+            if (post != null)
+            {
+                return View(new PostViewModel()
+                {
+                    PostId = post.Id,
+                    Title = post.Title,
+                    Content = ParseBBCode(post.Content)
+                });
+            }
+
+            return StatusCode(404);
+        }
+
+        public string ParseBBCode(string bbcode)
+        {
+            var parser = new BBCodeParser(new[]
+                {
+                    new BBTag("img", "<img src=\"${content}\" />", "", false, true),
+                    new BBTag("b", "<strong>", "</strong>"),
+                    new BBTag("color","<font  color=\"${color}\">","</font >", new BBAttribute("color", ""), new BBAttribute("color", "color")),
+                    new BBTag("i", "<span style=\"font-style:italic;\">", "</span>"),
+                    new BBTag("u", "<span style=\"text-decoration:underline;\">", "</span>"),
+                    new BBTag("code", "<pre class=\"prettyprint\">", "</pre>"),
+                    new BBTag("quote", "<blockquote>", "</blockquote>"),
+                    new BBTag("list", "<ul>", "</ul>"),
+                    new BBTag("*", "<li>", "</li>", true, false),
+                    new BBTag("url", "<a href=\"${href}\">", "</a>", new BBAttribute("href", ""), new BBAttribute("href", "href")),
+                    new BBTag("youtube", "<div class='video'><iframe width='550px' height='309px' src='//www.youtube.com/embed/${content}' allowFullScreen></iframe></div>","", false, true),
+                });
+            return parser.ToHtml(bbcode);
         }
 
         public IActionResult Carousel()
---
+++ CSM.Bataan.Web/ViewModels/PostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSM.Bataan.Web.ViewModels
+{
+    public class PostViewModel
+    {
+        public Guid? PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
