16752a469a593278faa95fb34cdb72d0cf1715f2
Goshen Jimenez <goshenjimenez@gmail.com>
2018-10-26 13:25:10 +0800

    More sophisticated data initializer

---
+++ CSM.Bataan.Web/ViewModels/Posts/PostImportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSM.Bataan.Web.ViewModels.Posts
+{
+    public class PostImportViewModel
+    {
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string Description { get; set; }
+
+        public string Title { get; set; }
+    }
+}
